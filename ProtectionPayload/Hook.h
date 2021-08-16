df3a450c63bf41099e9c0a5c7b74645e11f83b1f
Andro <hs554554@gmail.com>
2021-08-16 20:39:30 +0200

    Remove old libs

--- ProtectionPayload/Hook.h
+++ ProtectionPayload/Hook.h
@@ -7,9 +7,6 @@
 #include <polyhook2/Detour/x86Detour.hpp>
 #include <polyhook2/Detour/x64Detour.hpp>
 
-#pragma comment(lib, "capstone.lib")
-#pragma comment(lib, "PolyHook_2.lib")
-
 #include "Server.h"
 
 extern std::unique_ptr<Server> server;
