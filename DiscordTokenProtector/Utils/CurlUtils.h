5a39fa8ec3e2fa3df62fe61aa160dfb4e1313ba9
Andro <hs554554@gmail.com>
2021-08-16 20:02:50 +0200

    Removed old lib thingy

--- DiscordTokenProtector/Utils/CurlUtils.h
+++ DiscordTokenProtector/Utils/CurlUtils.h
@@ -1,7 +1,6 @@
 #pragma once
 #define CURL_STATICLIB
 #include <curl/curl.h>
-#pragma comment(lib, "libcurl_a.lib")
 #pragma comment(lib, "ws2_32.lib")
 #pragma comment(lib, "Wldap32.lib")
 #pragma comment(lib, "crypt32")
