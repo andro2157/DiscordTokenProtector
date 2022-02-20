f1ea7665c34c2d0077a19eb2e2bfed20d0d17d21
Andro <admin@andro24.host>
2022-02-20 19:54:05 +0100

    Added experimental thread protection

--- DiscordTokenProtector/Protection/ProcessProtection.h
+++ DiscordTokenProtector/Protection/ProcessProtection.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "../Includes.h"
+#include <polyhook2/CapstoneDisassembler.hpp>
+#include <polyhook2/Detour/x86Detour.hpp>
 
 class ProcessProtection {
 public:
 	ProcessProtection();
 	~ProcessProtection();
 
-	bool ProtectProcess(HANDLE hProcess = nullptr);
+	bool ProtectProcess(HANDLE hProcess = nullptr, bool protectThreads = true);
+	bool HookCreateThread();
+	bool setHandleSecurityInfo(HANDLE handle);
 
-	//TODO add thread protection, IAT hooks
+	//TODO add IAT hooks
 	/*
 	http://www.rohitab.com/discuss/topic/41701-how-to-stop-openprocess-without-inject-all-processes/
 	ZwTerminateProcess
@@ -22,9 +26,15 @@ public:
 
 	*/
 
+	uint64_t getOriginalCreateThread() const { return m_originalCreateThread; };
+
 private:
 	HANDLE m_hProcess;
+	DWORD m_pid;
 	HANDLE m_appMutex;
+
+	std::unique_ptr<PLH::x86Detour> m_detourCreateThread;
+	uint64_t m_originalCreateThread;
 };
 
 inline std::unique_ptr<ProcessProtection> g_processprotection;
