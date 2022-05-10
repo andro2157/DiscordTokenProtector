ba63b184d206e7d9c220f6baaabd191e97cb08b4
Andro <admin@andro24.host>
2022-05-10 18:50:16 +0200

    Update version

--- DiscordTokenProtector/Includes.h
+++ DiscordTokenProtector/Includes.h
@@ -27,7 +27,7 @@ __forceinline void FATALERROR_STR(std::string str) {
 	FATALERROR(str.c_str());
 }
 
-#define VER "dev-9"
+#define VER "dev-9-fix"
 
 #pragma comment(lib, "OpenGL32.lib")
 #pragma comment(lib, "crypt32.lib")
